4e92237be2b7f062302e345f8c1490939579c9dd
RyanCri <N00221108@iadt.ie>
2024-03-11 01:17:45 +0000

    added item seeder to test item functionality

---
+++ app/Http/Controllers/User/ItemController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Artist;
+use Auth;
+
+class ItemController extends Controller
+{
+    public function index() {
+        
+    }
+}
---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Item;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    protected $model = Item::class;
+    
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'expiry_date' => fake()->date(),
+            'icon_color' => fake()->hexColor(),
+            'user_id' => fake()->numberBetween(1,3),
+        ];
+    }
+}
